b3446e93da69a9528b8b3f689fc6aad068e9776a
tacheraSasi <tacherasasi@gmail.com>
2026-01-21 22:43:56 +0300

    refactor: reorganize imports and enhance authentication handling in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { ClerkProvider } from '@clerk/clerk-expo'
 
 
 export const unstable_settings = {
@@ -13,16 +13,22 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const { isSignedIn } = useAuth()
 
   return (
     <ClerkProvider tokenCache={tokenCache}>
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Protected guard={isSignedIn!}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack.Protected>
+          <Stack.Protected guard={!isSignedIn!}>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack.Protected>
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
     </ClerkProvider>
   );
 }
