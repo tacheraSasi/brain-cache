de9dc166582c2a8927afd3e1a3ee87bd40671b21
tacheraSasi <tacherasasi@gmail.com>
2026-01-22 00:03:24 +0300

    refactor: extract AppProvider component for improved layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,26 +16,33 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const { isSignedIn } = useAuth()
-
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <Stack>
-            <Stack.Protected guard={isSignedIn!}>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack.Protected>
-            <Stack.Protected guard={!isSignedIn!}>
-              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            </Stack.Protected>
-            <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-          </Stack>
-          <StatusBar style="auto" />
-        </ThemeProvider>
-      </ConvexProviderWithClerk>
+      <AppProvider />
     </ClerkProvider>
+  );
+}
+
+
+const AppProvider = () => {
+
+  const colorScheme = useColorScheme();
+  const { isSignedIn } = useAuth()
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Protected guard={isSignedIn!}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack.Protected>
+          <Stack.Protected guard={!isSignedIn!}>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack.Protected>
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </ConvexProviderWithClerk>
   );
 }
