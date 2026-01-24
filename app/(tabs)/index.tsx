b0c0a584f9b6bfb9f99c1ecdf8597fac0516d3fd
tacheraSasi <tacherasasi@gmail.com>
2026-01-24 23:56:57 +0300

    feat: wrap main view in SafeAreaView and add Content component for authenticated messages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,36 @@
 import { SignOutButton } from '@/components/signout'
+import { api } from '@/convex/_generated/api'
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
+import { useQuery } from 'convex/react'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Page() {
   const { user } = useUser()
 
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <SignedIn>
+          <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+          <SignOutButton />
+        </SignedIn>
+        <SignedOut>
+          <Link href="/(auth)/sign-in">
+            <Text>Sign in</Text>
+          </Link>
+          <Link href="/(auth)/sign-up">
+            <Text>Sign up</Text>
+          </Link>
+        </SignedOut>
+      </View>
+    </SafeAreaView>
   )
+}
+
+
+function Content() {
+  const messages = useQuery(api.messages.getForCurrentUser);
+  return <Text>Authenticated content: {messages?.length}</Text>;
 }
